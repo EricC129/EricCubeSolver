36a9ce4e2877fe49b47d957f616897116aeea16c
randomcuber <chubbylarry@gmail.com>
2026-08-13 20:57:05 -0700

    tuning offsets

--- cubesolver/src/main/java/org/cubexell/cubesolver/core/OpenCvRaspberryPiCamera.java
+++ cubesolver/src/main/java/org/cubexell/cubesolver/core/OpenCvRaspberryPiCamera.java
@@ -191,8 +191,8 @@ public class OpenCvRaspberryPiCamera implements CubeColorInspector{
 
     }
 
-    int offsetX = -100+50-200;
-    int offsetY = -200;
+    int offsetX = -100+50-200+50;
+    int offsetY = -200+50;
 
     public char[][] inspectBackFace(char center) {
         face = 0;
